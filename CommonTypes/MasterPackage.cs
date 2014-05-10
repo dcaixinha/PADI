aacb859d30b8ae14503389cc55b23b164f52948c
kurtteal <granchinhojp@msn.com>
2014-05-10 16:24:29 +0100

    - Added MasterPackage that was left behind by mistake from the last commit;

---
+++ CommonTypes/MasterPackage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PADI_DSTM
+{
+    [Serializable]
+    public class MasterPackage
+    {
+        private SortedDictionary<int, ServerInfo> servers;
+        private string serverWhoTransfers;
+
+        public MasterPackage(SortedDictionary<int, ServerInfo> servers, string server)
+        {
+            this.servers = servers;
+            serverWhoTransfers = server;
+        }
+
+        public SortedDictionary<int, ServerInfo> getServers()
+        {
+            return servers;
+        }
+
+        public string getServerWhoTransfers()
+        {
+            return serverWhoTransfers;
+        }
+    }
+}
